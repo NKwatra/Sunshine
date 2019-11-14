d3beea768c5f6bbe262cffe0b5475ff3997e5f43
NKwatra <kwatranishkarsh@gmail.com>
2019-11-14 14:58:23 +0530

    show loading indicator and error messages when error loading data

--- App.js
+++ App.js
@@ -1,36 +1,75 @@
 import React from "react";
-import { StyleSheet, Text, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  ScrollView,
+  ProgressBarAndroid,
+  View,
+  StatusBar
+} from "react-native";
 import API from "./API";
 import parseData from "./Utilities/formatWeather";
 
 export default class App extends React.Component {
   state = {
-    weatherForcast: []
+    weatherForcast: [],
+    loading: true,
+    error: false
   };
   componentDidMount() {
     this.fetchWeatherData();
   }
   render() {
-    return (
-      <ScrollView style={styles.container}>
-        {this.state.weatherForcast.map(dailyForcast => (
-          <Text key={dailyForcast} style={styles.forecast}>
-            {dailyForcast}
+    if (this.state.loading) {
+      return (
+        <View style={styles.centredContent}>
+          {/* TODO : Find a better alternative to show loading on ios devices */}
+          <StatusBar networkActivityIndicatorVisible={this.state.loading} />
+          <ProgressBarAndroid
+            color="red"
+            animating={this.state.loading}
+            style={styles.progressBar}
+          />
+        </View>
+      );
+    } else if (this.state.error) {
+      return (
+        <View style={styles.centredContent}>
+          <Text>
+            Oops! there seems to be an error loading data, please check your
+            internet connection
           </Text>
-        ))}
-      </ScrollView>
-    );
+        </View>
+      );
+    } else {
+      return (
+        <ScrollView style={styles.container}>
+          {this.state.weatherForcast.map(dailyForcast => (
+            <Text key={dailyForcast} style={styles.forecast}>
+              {dailyForcast}
+            </Text>
+          ))}
+        </ScrollView>
+      );
+    }
   }
 
   async fetchWeatherData() {
-    const key = API[0];
-    const response = await fetch(
-      `https://api.weatherbit.io/v2.0/forecast/daily?city=Delhi&key=${key}`
-    );
-    const { data } = await response.json();
-    this.setState({
-      weatherForcast: parseData(data)
-    });
+    try {
+      const key = API[0];
+      const response = await fetch(
+        `https://api.weatherbit.io/v2.0/forecast/daily?city=Delhi&key=${key}`
+      );
+      const { data } = await response.json();
+      this.setState({
+        weatherForcast: parseData(data),
+        loading: false
+      });
+    } catch {
+      this.setState({
+        error: true
+      });
+    }
   }
 }
 
@@ -42,5 +81,10 @@ const styles = StyleSheet.create({
   forecast: {
     padding: 10,
     fontSize: 20
+  },
+  centredContent: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
