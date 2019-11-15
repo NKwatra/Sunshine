8acb21eb2fdbe8a687eff0063e449626d6e8664e
NKwatra <kwatranishkarsh@gmail.com>
2019-11-15 12:21:23 +0530

    create very basic settings UI

--- screens/settingsScreen/SettingsScreen.js
+++ screens/settingsScreen/SettingsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Switch } from "react-native";
 import BottomNavigationIcon from "../../BottomNavigationIcon";
 
 export default class SettingsScreen extends React.Component {
@@ -7,11 +7,50 @@ export default class SettingsScreen extends React.Component {
         tabBarIcon: <BottomNavigationIcon name="settings" />
     };
 
+    state = {
+        units: "Metric",
+        currentLocation: true,
+        location: ""
+    };
+
     render() {
         return (
-            <View>
-                <Text> Hey</Text>
+            <View style={styles.container}>
+                <TouchableOpacity style={styles.row}>
+                    <Text>Units</Text>
+                    <Text>{this.state.units}</Text>
+                </TouchableOpacity>
+                <View style={styles.line}></View>
+                <View style={styles.row}>
+                    <Text>Use Current Location</Text>
+                    <Switch
+                        value={this.state.currentLocation}
+                        onValueChange={value =>
+                            this.setState({ currentLocation: value })
+                        }
+                    />
+                </View>
+                <View style={styles.line}></View>
+                {this.state.currentLocation ? null : (
+                    <TouchableOpacity style={styles.row}>
+                        <Text>Location</Text>
+                        <Text>{this.state.location}</Text>
+                    </TouchableOpacity>
+                )}
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 40
+    },
+    line: {
+        height: 2,
+        backgroundColor: "gray"
+    },
+    row: {
+        flexDirection: "row"
+    }
+});
